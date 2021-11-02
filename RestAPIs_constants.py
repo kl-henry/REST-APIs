48f8d58bded683fbdba9a696fdf9badfc825d702
Heinz Hill <hz.hill@web.de>
2021-11-02 10:04:43 +0100

    remove personal API Keys

--- RestAPIs_constants.py
+++ RestAPIs_constants.py
@@ -9,12 +9,5 @@ keyword_switcher = {
 
 FF = 'ff'
 
-NASA_API_KEY = "GpfT2Z1sqB9BMiGRfduTf6Bh540MmlLDVNo8lYIi"
-
 IMAGE_BASE = 60
-
-OMDB_API_KEY = "9731838b"
-
-SPOONACULAR_API_KEY = "79e1c8c0b27f40d9b84e492dd60903dd"
-
 
